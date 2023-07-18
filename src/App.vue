738e5e256729dcea4efb2a4121b1b4a21a262017
Wietze Duursma <w.duursma@outlook.com>
2023-07-18 21:16:53 +0200

    change text

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const movies = reactive(items);
 
 <template>
   <div class="mx-auto max-w-2xl py-16">
-    <h2 class="text-2xl font-bold text-white">Movie's</h2>
+    <h2 class="text-2xl font-bold text-white">Movies</h2>
 
     <div
       class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
